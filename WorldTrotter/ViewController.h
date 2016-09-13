9d4e742844ec38deeed3b8ce74aec71ea839d340
Daniel Kwolek <Daniel@daniels-macbook-pro.local>
2016-09-13 04:00:01 -0400

    Initial Commit

---
+++ WorldTrotter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorldTrotter
+//
+//  Created by Daniel Kwolek on 9/13/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WorldTrotter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WorldTrotter
+//
+//  Created by Daniel Kwolek on 9/13/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
