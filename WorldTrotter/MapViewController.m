4217c405472269153c83959362fe8d418bd74b30
Daniel Kwolek <Daniel@Daniels-MacBook-Pro.local>
2016-09-16 10:44:06 -0400

    Chapter 6 initial commit

--- WorldTrotter/MapViewController.m
+++ WorldTrotter/MapViewController.m
@@ -18,41 +18,6 @@
 
 @implementation MapViewController
 
-- (void)loadView {
-    self.mapView = [MKMapView new];
-    
-    self.view = self.mapView;
-    
-    // Add a segmented control to the view
-    NSArray *segItems = @[ @"Standard", @"Hybrid", @"Satellite" ];
-    UISegmentedControl *segControl = [[UISegmentedControl alloc] initWithItems:segItems];
-    segControl.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
-    segControl.selectedSegmentIndex = 0;
-    
-    
-    segControl.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:segControl];
-    
-    
-    [segControl addTarget:self
-                   action:@selector(mapTypeChanged:)
-         forControlEvents:UIControlEventValueChanged];
-    
-    
-    NSLayoutConstraint *topConstraint =
-    [segControl.topAnchor constraintEqualToAnchor:self.topLayoutGuide.bottomAnchor constant:8];
-    
-    UILayoutGuide *margins = self.view.layoutMarginsGuide;
-    NSLayoutConstraint *leadingConstraint =
-    [segControl.leadingAnchor constraintEqualToAnchor:margins.leadingAnchor];
-    NSLayoutConstraint *trailingConstraint =
-    [segControl.trailingAnchor constraintEqualToAnchor:margins.trailingAnchor];
-    
-    topConstraint.active = YES;
-    leadingConstraint.active = YES;
-    trailingConstraint.active = YES;
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"MapViewController loaded its view.");
@@ -61,22 +26,6 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-
-- (void)mapTypeChanged:(UISegmentedControl *)sender {
-    switch (sender.selectedSegmentIndex) {
-        case 0:
-            self.mapView.mapType = MKMapTypeStandard;
-            break;
-        case 1:
-            self.mapView.mapType = MKMapTypeHybrid;
-            break;
-        case 2:
-            self.mapView.mapType = MKMapTypeSatellite;
-        default:
-            break;
-    }
 }
 
 
